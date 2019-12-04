402491a249179c6ccbb748ebeed6555d25bbd3aa
posakya <roshanposakya07@gmail.com>
2019-12-04 13:30:17 +0545

    hello its second

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,9 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("hello ");
+            
+        System.out.println("Hello i am a programmers");
         // TODO code application logic here
     }
     
